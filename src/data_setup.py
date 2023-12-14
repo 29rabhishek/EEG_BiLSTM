54c5582c3da74b578c6365e6c4a36936da8f74aa
Abhishek Rathore <29rabhishek@gmail.com>
2023-12-14 12:04:28 +0530

    Fix the train_test_dataloader for SAMPLE TEST DATA ONLY

--- src/data_setup.py
+++ src/data_setup.py
@@ -1,9 +1,36 @@
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import random 
 
-def train_test_dataloader(X, y, test_size):
-    train_idx = int(X.shape[0]*(1-test_size))
-    test_idx = X.shape[0] - train_idx
-    return 
+def train_test_dataloader(X, y, test_frac, batch_size):
+    """
+    this function split X into train and test data using test_frac for test size
+    then return train_dataloader and test_dataloader type(DataLoader)
+
+    Parameter
+    X: tensor
+    y: tensor
+    test_frac:  float in range (0,1)
+    batch_size: int 
+
+    Return
+    train_dataloader: torch.utils.data.DataLoader
+    test_dataloader: torch.utils.data.DataLoader
+    """
+    test_size = int(X.shape[0]*(test_frac))
+    train_size = X.shape[0] - test_size
+
+    idx = range(0, X.shape[0])
+    train_idx = random.sample(idx, train_size)
+    test_idx = list(set(idx) - set(train_idx))
+
+    train_dataset = eeg_dataset(X[train_idx], y[train_idx])
+    test_dataset = eeg_dataset(X[test_idx], y[test_idx])
+
+    train_dataloader = DataLoader(train_dataset, batch_size = batch_size)
+    test_dataloader = DataLoader(test_dataset, batch_size = batch_size)
+    print(f"Train Size: {train_size} || Test Size: {test_size} ")
+    return train_dataloader, test_dataloader
     
 
 class eeg_dataset(Dataset):
